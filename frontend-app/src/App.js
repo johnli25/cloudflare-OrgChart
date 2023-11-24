b94fff626ccf39c8a8f647a9e328dd0ea804c877
Li <johnwl2@illinois.edu>
2023-11-24 01:25:18 -0600

    Remove accidental submodules and add comments

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-// src/App.js
 import React from 'react';
 import OrganizationChart from './OrganizationChart';
 
@@ -9,7 +8,7 @@ function App() {
       <div className="App">
           <header className="App-header">
               <h1>The Organization Chart</h1>
-              <OrganizationChart /> {/* Use the component */}
+              <OrganizationChart /> {/* Utilize component the component */}
           </header>
       </div>
   );
